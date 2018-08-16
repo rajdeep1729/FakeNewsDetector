e8fb33442d66e26edc5305004fa51023b7a4765a
Rajdeep <rajdeepmaity03@gmail.com>
2018-08-16 21:09:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rajdeep/fakenewsdetetor/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.rajdeep.fakenewsdetetor;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rajdeep.fakenewsdetector.R;
+
+public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,FirstActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
