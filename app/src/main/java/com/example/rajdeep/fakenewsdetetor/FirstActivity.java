d0fb5e1abdfcc7bef87f01a0c1e4ca7ac03d66f0
Rajdeep <rajdeepmaity03@gmail.com>
2018-08-17 13:09:40 +0530

    second

--- app/src/main/java/com/example/rajdeep/fakenewsdetetor/FirstActivity.java
+++ app/src/main/java/com/example/rajdeep/fakenewsdetetor/FirstActivity.java
@@ -2,6 +2,10 @@ package com.example.rajdeep.fakenewsdetetor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rajdeep.fakenewsdetector.R;
 
@@ -11,7 +15,19 @@ public class FirstActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        final EditText t=findViewById(R.id.editText);
+        Button btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener()
 
+        {
+            @Override
+            public void onClick (View v){
+
+                String message=t.getText().toString();
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
     }
 }
