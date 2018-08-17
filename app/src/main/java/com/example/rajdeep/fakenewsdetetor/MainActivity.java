d5ea61ebe97db30835b7c29f1909d8f1c03e7711
Dipankar-minj <gonthestoneageboy@gmail.com>
2018-08-17 18:53:22 +0530

    third

--- app/src/main/java/com/example/rajdeep/fakenewsdetetor/MainActivity.java
+++ app/src/main/java/com/example/rajdeep/fakenewsdetetor/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import com.example.rajdeep.fakenewsdetector.R;
 
 public class MainActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
