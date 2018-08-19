aefc7568a7aa718367dea836a69f8f13fa5ddc39
Dipankar-minj <gonthestoneageboy@gmail.com>
2018-08-19 20:40:06 +0530

    third

--- app/src/main/java/com/example/rajdeep/fakenewsdetetor/MainActivity.java
+++ app/src/main/java/com/example/rajdeep/fakenewsdetetor/MainActivity.java
@@ -4,15 +4,29 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.rajdeep.fakenewsdetector.R;
 
 public class MainActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView  = findViewById(R.id.splashscreen);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anim);
+        imageView.startAnimation(animation);
+        ImageView imageView1  = findViewById(R.id.brand);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anim);
+        imageView1.startAnimation(animation1);
 
         new Handler().postDelayed(new Runnable(){
 
